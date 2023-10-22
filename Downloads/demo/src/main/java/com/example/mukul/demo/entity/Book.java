a0d0a87b5832ff70c24c0a962213674e9ea00d20
Mukul Vasanth <mukulvasanth@gmail.com>
2023-10-22 12:07:49 +0530

    BookLibrabry backend

---
+++ Downloads/demo/src/main/java/com/example/mukul/demo/entity/Book.java
@@ -0,0 +1,67 @@
+package com.example.mukul.demo.entity;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "book")
+public class Book {
+    private int id;
+    @NotEmpty(message = "title shouldn't be null")
+    private String title;
+    @NotEmpty (message = "author shouldn't be null")
+    private String author;
+    @NotEmpty (message = "category shouldn't be null")
+    private String category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Min(0)
+    @Max(10)
+    private int rating;
+}
